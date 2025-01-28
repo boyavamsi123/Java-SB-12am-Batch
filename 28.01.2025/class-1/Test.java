d06e9890f6a97ffa9659a883ae7d394388627b47
boyavamsi123 <vamvamshi56@gmail.com>
2025-01-28 19:25:18 +0530

    txt

---
+++ 28.01.2025/class-1/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void login(){
+        System.out.println("Login Successfully...");
+    }
+    public static void main(String[] args){
+        login();
+    }
+}
---
+++ 28.01.2025/practice/class-2/variable.java
@@ -0,0 +1,6 @@
+class variable{
+    public static void main(String[] args){
+        int num = 10;  
+        System.out.println(" Variable: " + num);  
+    }
+}
