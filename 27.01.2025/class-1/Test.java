b29ec5df1f41c623a92f2ca2b1c85bb4d8bd9214
boyavamsi123 <vamvamshi56@gmail.com>
2025-01-28 10:55:16 +0530

    text

---
+++ 27.01.2025/class-1/Test.java
@@ -0,0 +1,20 @@
+class A{
+     public static void main(String[] args){
+        System.out.println("Hello..");
+     }
+}
+class B{
+     public static void main(String[] args){
+        System.out.println("Hi..");
+     }
+}
+class C{ 
+     public static void main(String[] args){
+        System.out.println("How are you");
+     }
+    }
+class D{
+     public static void main(String[] args){
+        System.out.println("Fine");
+     }
+}
