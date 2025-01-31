5c0958f9c9461acb7af607565b29befd4b646bdf
boyavamsi123 <vamvamshi56@gmail.com>
2025-01-31 11:54:19 +0530

    hi

---
+++ 31.01.2025/practice/Uninary.java
@@ -0,0 +1,6 @@
+class Uninary{
+    public static void main(String[] args){
+        int a=100,b=10,c;
+        System.out.println("Uninary: "+(a++/++a*++b+10));
+    }
+}
---
+++ 31.01.2025/practice/operators.java
@@ -0,0 +1,25 @@
+class operators{
+    public static void main(String[] args){
+        int a=10;
+        int b=3;
+        System.out.println("Arithematical Operators");
+        System.out.println("Addition: "+(a+b));
+        System.out.println("Subtraction: "+(a-b));
+        System.out.println("Multiplication: "+(a*b));
+        System.out.println("Division: "+(a/b));
+        System.out.println("Remainder: "+(a%b));
+        System.out.println("Uninary + operator: "+(+a));
+        System.out.println("Uninary - operator: "+(-a));
+        System.out.println("Uninary Increment Operators: "+(++a));
+        System.out.println("Uninary Decrement Operators: "+(--a));
+        System.out.println("Uninary Increment Operators: "+(++a));
+        System.out.println("Uninary Increment Operators: "+(a++));
+        System.out.println("Uninary Increment Operators: "+(a++));
+        System.out.println("Uninary Increment Operators: "+(a++));
+        System.out.println("Uninary + operator: "+(+a))
+        // ++a  pre increment   
+        // a++  post increment
+        // --a  pre decrement
+        // a--  post decrement  
+    }
+}
