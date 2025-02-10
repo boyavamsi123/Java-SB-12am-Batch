65c8b7274c8d36e3c14eacce193f2082d9ecdc8b
boyavamsi123 <vamvamshi56@gmail.com>
2025-02-10 11:20:30 +0530

    txt

---
+++ practise/practice-7/Test.java
@@ -0,0 +1,8 @@
+class Main{
+     public static void main(String[] args){
+        String[] cars = {"Volvo","BMW","Scorpio","Suzuki","Furtuner"};
+        for(int i = 0; i<cars.length; i++){
+            System.out.println(cars[i]);
+        }
+     }
+}
---
+++ practise/practice-8/Array.java
@@ -0,0 +1,12 @@
+class Array{
+    public static void main(String[] args) {
+        int number = 5;
+
+// Find out if the number above is even or odd
+if (number % 2 == 0) {
+  System.out.println(number + " is even.");
+} else {
+  System.out.println(number + " is odd.");
+}
+    }
+}
