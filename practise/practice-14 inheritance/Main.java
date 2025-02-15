66c44b1f2d8dddb7eb14d731cff89dcb9c244533
boyavamsi123 <vamvamshi56@gmail.com>
2025-02-15 22:43:50 +0530

    txt

---
+++ practise/practice-14 inheritance/Main.java
@@ -0,0 +1,32 @@
+class Data{
+    int s=10;
+    int l=20;
+    int b=30;
+    int s1=25, s2=35, s3=42;
+}
+class square extends Data{
+    int area;
+    void calarea(){
+        area = s*s;
+        System.out.println(area);
+    }
+}
+
+Class Rectangle extends Data{
+    int p, area;
+    void calculate(){
+        p=2*(l+b);
+        area=l*b;
+        System.out.println("parameter :"+p);
+        System.out.println("area :"+area);
+    }
+}
+
+class main{
+    public static void main(String[] args){
+        Square s = new square();
+        Rectangle r = new Rectangle();
+        S.calarea();
+        r.calculate();
+    }
+}
