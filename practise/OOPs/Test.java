b7bb10655287b780af0b6d2d2a4b6f15f35fa5ec
boyavamsi123 <vamvamshi56@gmail.com>
2025-02-23 20:01:51 +0530

    txt

---
+++ practise/OOPs/Test.java
@@ -0,0 +1,7 @@
+class Test{
+
+}
+new Test();
+new Test();
+new Test();
+new Test();
---
+++ practise/Polymorphism/EncapTest.java
@@ -0,0 +1,26 @@
+
+
+public class EncapTest{
+    private String name;
+    private String idNum;
+    private int age;
+
+    public int getAge(){
+        return age;
+    }
+    public String getname(){
+        return name;
+    }
+    public String getIdNum(){
+        return idNum;
+    }
+    public void setage(int newage){
+        age = newage;
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setIdNum(String newId){
+        idNum = newId;
+    }
+}
